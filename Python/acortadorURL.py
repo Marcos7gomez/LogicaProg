98d6dc9d7fa2276daede6ee535928a5304c6ea86
Marcos Gomez <marcoslg.gomez@gmail.com>
2024-03-05 20:58:32 -0300

    add acortador de url funcionando con streamlit

--- Python/acortadorURL.py
+++ Python/acortadorURL.py
@@ -1,2 +1,15 @@
 import pyshorteners
-import streamlit
+import streamlit as st
+
+def shorten_url(url):
+    s = pyshorteners.Shortener()
+    shortened_url = s.tinyurl.short(url)
+    return shortened_url
+
+# Creo pagina con streamlit
+st.set_page_config(page_title="Acortador de URL", page_icon="👀", layout="centered")
+st.image("image/py.png", use_column_width=True)
+st.title("Acortador de url")
+url = st.text_input("Agregá la URL")
+if st.button("Generar nueva URL"):
+    st.write("URL acortada: ", shorten_url((url)))
